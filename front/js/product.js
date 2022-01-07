41e3ad924cf96375e1226de99356d07fe47623d8
robinson escoulen <robi.escoulen@gmail.com>
2022-01-07 14:32:51 +0100

    URLSearchParams

---
+++ front/js/product.js
@@ -0,0 +1,8 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const id = urlParams.get("id");
+console.log({ id });
+
+fetch(`http://localhost:3000/api/products/${id}`)
+  .then((res) => res.json())
+  .then((res) => console.log(res));
