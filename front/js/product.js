c9c028bd920cd4fd91443790b03dc076222b5a3a
robinson escoulen <robi.escoulen@gmail.com>
2022-01-10 10:12:53 +0100

    product img

--- front/js/product.js
+++ front/js/product.js
@@ -5,4 +5,26 @@ console.log({ id });
 
 fetch(`http://localhost:3000/api/products/${id}`)
   .then((res) => res.json())
-  .then((res) => console.log(res));
+  .then((res) => getData(res));
+
+//recuperation des données
+function getData(kanap) {
+  // const altTxt = kanap.altTxt;
+  // const colors = kanap.colors;
+  // const description = kanap.description;
+  // const imageUrl = kanap.imageUrl;
+  // const name = kanap.name;
+  // const price = kanap.price;
+  // const _id = kanap._id;
+  const { altTxt, colors, description, imageUrl, name, price, _id } = kanap;
+  makeImage(imageUrl, altTxt);
+}
+
+//affichage de l'image
+function makeImage(imageUrl, altTxt) {
+  const image = document.createElement("img");
+  image.src = imageUrl;
+  image.alt = altTxt;
+  const parent = document.querySelector(".item__img");
+  if (parent != null) parent.appendChild(image);
+}
